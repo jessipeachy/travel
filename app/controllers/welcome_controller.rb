64b870333cd5a75ea40b9d70f17dcd358184197c
Jessi King <jessica.l.king86@gmail.com>
2016-02-01 20:01:15 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homeland = 'USA'
+  	@countries = ['New Zealand', 'Ireland', 'United Kingdom', 'Scotland', 'Italy']
+  	@country_images = ['ireland.jpg', 'UK.png', 'italy.jpg', 'new-zealand.jpg']
+  end
+
+  def about
+  	@color = params[:color]
+  	@size = params[:size].to_i
+  end
+end
